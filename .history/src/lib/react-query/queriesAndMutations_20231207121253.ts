16f05423541c588b168f86e064c816ab22bbacc6
Tracy-maker <ydlvns@gmail.com>
2023-12-07 14:35:01 +1100

    create post function

---
+++ .history/src/lib/react-query/queriesAndMutations_20231207121253.ts
@@ -0,0 +1,36 @@
+import {
+  useMutation,
+  useQueryClient,
+  useInfiniteQuery,
+  useQuery,
+} from "@tanstack/react-query";
+
+import {
+  createUserAccount,
+  signInAccount,
+  signOutAccount,
+} from "../appwrite/api";
+import { INewUser } from "@/types";
+
+export const useCreateUserAccount = () => {
+  return useMutation({
+    mutationFn: (user: INewUser) => createUserAccount(user),
+  });
+};
+
+export const useSignInAccount = () => {
+  return useMutation({
+    mutationFn: (user: { email: string; password: string }) =>
+      signInAccount(user),
+  });
+};
+
+export const useSignOutAccount = () => {
+  return useMutation({
+    mutationFn: signOutAccount,
+  });
+};
+
+export const useCreatePost = () => {
+  const queryClient = useQueryClient();
+};
