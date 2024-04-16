3ef0b148b69bb81ac062c187afe1b51c534ed9d6
Tracy-maker <ydlvns@gmail.com>
2024-04-16 18:27:26 +1000

    adding files

--- .history/src/_auth/AuthLayout_20231206180149.tsx
+++ .history/src/_auth/AuthLayout_20231206180149.tsx
@@ -21,7 +21,7 @@ const AuthLayout = () => {
             className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat rounded-xl"
           >
             <source
-              src="https://videos.pexels.com/video-files/6853337/6853337-hd_720_1366_25fps.mp4"
+              src="https://player.vimeo.com/external/513373860.hd.mp4?s=3aa9a256b9e4eea8b6ed071af5cf6052c3d88c69&profile_id=174&oauth2_token_id=57447761"
               type="video/mp4"
             />
             Your browser does not support the video tag.
